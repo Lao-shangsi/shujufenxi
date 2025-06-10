946554086e68e472eae9c111177e4112d4655bee
Light Still Shines <2802064263@qq.com>
2025-06-10 11:33:38 +0800

    更改配置

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // 如果仓库是 username.github.io 则 base: '/'
+  base: '/shujufenxi/', // 如果仓库是 username.github.io 则 base: '/'
 })
